ba9c4eb581866fa008e0d195354e14d4981df82f
Chris Lindelof <clindelof2013@my.fit.edu>
2016-04-04 08:12:41 -0400

    See changelog for details
    Changes made to Map.java, Graph.java and mapFile.txt

--- src/Graph.java
+++ src/Graph.java
@@ -18,28 +18,60 @@ public class Graph {
         }
     }
 
+    /**
+     * Get the number of vertices in the graph
+     * @return this.V - the number of vertices
+     */
     public int V(){
         return V;
     }
 
+    /**
+     * Get the number of edges in the graph.
+     * @return this.E - this number of edges
+     */
     public int E(){
         return E;
     }
     
+    /**
+     * Weights are stored in a 2D array. The weight of the connection between points 1 and 2
+     * is found by going to array[1][2];
+     * 
+     * @return - the double representation of the weight of the connection from 1 to 2.
+     */
     public double getWeight(int from, int to){
         return this.w[from][to];
     }
     
+    /**
+     * Sets the weight of the connection from point 1 to 2.
+     * 
+     * @param from - starting point
+     * @param to - ending point
+     * @param price - the weight of the connection
+     */
     public void setWeight(int from, int to, double price){
         this.w[from][to] = price;
     }
 
+    /**
+     * Add a weighted edge to the graph
+     * @param v1 - starting vertex
+     * @param v2 - ending vertex
+     * @param weight - weight of the connection
+     */
     public void addWeightedEdge(int v1, int v2, double weight){
         adj[v1].add(v2);
         w[v1][v2]= weight;
         E++;
     }
 
+    /**
+     * Get the points which are directly connected to the given point v
+     * @param v - vertex to get connections from
+     * @return - the ArrayList which represents the direct connections from the vertex v
+     */
     public ArrayList<Integer> adj(int v){
         return adj[v];
     }
--- src/Map.java
+++ src/Map.java
@@ -25,8 +25,17 @@ public class Map extends Graph{
 		}
 	}
 	
+	/**
+	 * Add the connection between two stations. Since tracks are able to be traversed in both directions,
+	 * this connection must be added from start -> end and end -> start.
+	 * 
+	 * @param startStation - vertex to start from
+	 * @param endStation - vertex to end at
+	 * @param weight - weight of the connection between start and end.
+	 */
 	void addEdge (int startStation, int endStation, int weight) {
-		
+		this.addWeightedEdge(startStation, endStation, weight);
+		this.addWeightedEdge(endStation, startStation, weight);
 	}
 	
 	void lock(Edge edge) {
