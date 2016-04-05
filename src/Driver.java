9e63d49958e8b6de261ea4b8f5506f9df7f1c4f1
Chris Lindelof <clindelof2013@my.fit.edu>
2016-04-05 00:56:51 -0400

    See changelog
    Changes made to structure as well as functionality improvements.

--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,13 @@
 
 public class Driver {
 	
-	public static final int NUMBEROFTRAINS = 13;
+	public static final int NUMBEROFSTATIONS = 13;
 	public static String inputSchedule;
 
 	public static void main(String[] args) {
 		
 		inputSchedule = args[0];
-		TrainScheduler schedule = new TrainScheduler(NUMBEROFTRAINS, inputSchedule);
+		TrainScheduler schedule = new TrainScheduler(NUMBEROFSTATIONS, inputSchedule);
 		
 		//System.out.println(schedule.trainMap.toString());
 	}
