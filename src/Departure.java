80db110454b50ccd3f11646fb810a62141a09d40
elarochelle2013 <elarochelle2013@my.fit.edu>
2016-04-06 19:35:44 -0400

    toString in Departure.Java Fuck this shit

--- src/Departure.java
+++ src/Departure.java
@@ -17,6 +17,50 @@ public class Departure implements Comparable<Departure>{
 		} else {
 			return 0;
 		}
+		
+		public String toString() {
+			
+			int levels = 0;		
+			int levelLimit = lineCount();	
+			int num = 1; 	
+			int numCount = 0;		
+			StringBuilder sb = new StringBuilder();	
+			
+			sb.append("Expected Departure " + elements + " elements\n");
+			
+			while (levels < levelLimit) {
+			
+				if (levels < 1) {
+					numCount = 1;
+				} else {
+					numCount = (int) Math.pow(levels, 2);
+					if (numCount == 1) {
+						numCount = 2;
+					}
+				}
+				
+				while (num <= elements && numCount > 0) {
+				
+					int tabs = levelLimit - levels;
+						
+						while (tabs > 0) {
+							--tabs;
+							sb.append("\t");
+						}
+					
+					sb.append(data[num].toString());
+					
+					num++;
+					numCount--;
+				}
+				
+				sb.append("\n");
+				levels++;
+			}
+		
+			return new String(sb);
+		}
+			
 	}
 	
 }
